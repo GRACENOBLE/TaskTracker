b4e61ad7e10b8b5d57ac7933b0203a2f95b93717
GRACENOBLE <gracenoble72@gmail.com>
2024-10-31 13:26:25 +0300

    feat(delete): can delete from terminal

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "time"
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -10,6 +11,7 @@ import (
 )
 
 func main() {
+
 	reader := bufio.NewReader(os.Stdin)
 
 	print("What would you like to do?\n")
@@ -23,15 +25,18 @@ func main() {
 		AppendToFile("hello.json", parts[1])
 
 	} else if parts[0] == "update" && parts[1] != "" && parts[2] != "" {
+
 		if len(parts) == 3 {
+
 			index, err := strconv.ParseInt(parts[1], 10, 64)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			//remember to give warning in case user accidentally creates a todo with name same as ststus enum type
+			//remember to give warning in case user accidentally creates a todo with name same as status enum type
 
 			UpdateTask("hello.json", index, parts[2], "")
+
 		} else if len(parts) == 4 {
 
 			index, err := strconv.ParseInt(parts[1], 10, 64)
@@ -40,7 +45,9 @@ func main() {
 			}
 
 			UpdateTask("hello.json", index, parts[2], parts[3])
+
 		}
+
 	} else if parts[0] == "mark" && parts[1] != "" && parts[2] != "" {
 
 		if len(parts) == 3 {
@@ -54,6 +61,24 @@ func main() {
 
 		}
 
+	} else if parts[0] == "delete" && parts[1] != ""{
+
+		fmt.Println("Are you sure?(y/n)")
+
+		var choice string
+		fmt.Scanf("%v", &choice)
+
+		if choice == "Y" || choice == "y"{
+
+			index, err := strconv.ParseInt(parts[1], 10, 64)
+			if err != nil{
+				log.Fatal(err)
+			}
+
+			DeleteTask("hello.json",index)
+
+		}
+			
 	}
 
 }
